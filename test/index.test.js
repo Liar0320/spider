a3b2e8c9d3075341de30054aaeda0a2c1b8361aa
lich <100108350@qq.com>
2019-10-17 22:30:16 +0800

    test: 添加测试模块,并且验证sprideBookChapters

---
+++ test/index.test.js
@@ -0,0 +1,1 @@
+require('./spiderBookChapters.test');
---
+++ test/spiderBookChapters.test.js
@@ -0,0 +1,14 @@
+const bookurl = '/book/1279/';
+const baseUrl = 'http://www.abcxs.com';
+const spiderBookChapters = require('../src/utils/spiderBookChapters');
+
+/**生成爬取小说地址 */
+function packBookChaptersUrl(bookurl) {
+    return baseUrl + bookurl;
+}
+
+spiderBookChapters(packBookChaptersUrl(bookurl)).then(chapters=>{
+    console.log(chapters);
+})
+
+
