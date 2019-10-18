c229ce6562ab8a84485bc239d573ad1e4f03b223
lich <100108350@qq.com>
2019-10-18 14:29:53 +0800

    feat: 根据对象池创建一个限制下载先线程的链接池

--- src/utils/connectionPool.js
+++ src/utils/connectionPool.js
@@ -1,40 +1,80 @@
 /*
  * @Author: lich 
- * @Date: 2019-10-17 22:58:07 
+ * @Date: 2019-10-18 10:19:23 
  * @Last Modified by: lich
- * @Last Modified time: 2019-10-17 23:09:43
+ * @Last Modified time: 2019-10-18 13:52:32
  * @TODO:
- * open:开启一个新的连接
- * close:关闭当前连接
- * isFull:连接池是否全部占用
- * improve设计
- * 当超时时自动关闭连接，防止长时间占用连接
+ * register：注册所有的待链接
+ * next：如果对象池中存在可用实例，则执行下一个链接
+ * start：开始启动下载链接
  */
-/**最大连接池为20 */
-const maxConnection = 20;
 
-const connectionCollection = [];
+const Pool = require("./Pool");
+const poolInstance = new Pool();
+
+const {spiderChapterInfo} = require('./spider');
+
+// class SpiderChapterInfo {
+//     start(chapterInfoUrl) {
+//         return spiderChapterInfo(chapterInfoUrl)
+//     }
+// }
+new Array(poolInstance.maxConnection).fill(null).forEach(()=>{
+    // let instance = poolInstance.instantiate(SpiderChapterInfo);
+    poolInstance.put(spiderChapterInfo);
+});
+
+const store = [];
+
+function noop() {
+    return void 0;
+}
 
 class ConnectionPool {
-    /**
-     * 
-     * @param { Promise } __connection 
-     */
-    open(__connection) {
-       let connection = new Connection(__connection);
+    register(waitConnection, callback ) {
+        this.waitConnection = waitConnection || [];
+        this.queryConnectionDoneId = null;
+        this.callback = callback || noop ;
+    }
+    next() {
+        console.log(poolInstance.size());
+        if (this.waitConnection.length > 0 && poolInstance.size() > 0) {
+           let instance = poolInstance.get();
+           let params = this.waitConnection.shift();
 
-        
-return connection
+           instance(params.url).then(content=>{
+            poolInstance.put(instance);
+            store.push(Object.assign(params, {content: content}));
+           })
+        } else {
+            console.log("对象池被占用完，等待......................");
+        }
+    }
+    start() {
+        this.next();
+        this.queryConnectionDoneId = this.queryConnectionDone();
     }
     close() {
-
+        this.callback(store);
+        this.waitConnection = null;
+        this.queryConnectionDone = null;
+        this.callback = null;
     }
-    isFull() {
-       return connectionCollection.length >= maxConnection;
+    queryConnectionDone() {
+       let queryConnectionDoneId = setInterval(() => {
+            if (this.waitConnection.length === 0) {
+                if (poolInstance.size() === poolInstance.maxConnection) {
+                    clearInterval(queryConnectionDoneId);
+                    this.close();
+                }
+            } else {
+                this.next();
+            }
+        }, 100);
+        
+        return queryConnectionDoneId
     }
 }
 
-class Connection {
-
-}
+module.exports = ConnectionPool;
 
