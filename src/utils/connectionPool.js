0a90b04b923b99ee9a5d25e53fb88573f372d7ee
lich <100108350@qq.com>
2019-10-21 09:21:03 +0800

    improvement: 将对象池放在 connectionPool内，并且添加每有20个文件缓存就直接一次生成文件清空缓存

--- src/utils/connectionPool.js
+++ src/utils/connectionPool.js
@@ -2,7 +2,7 @@
  * @Author: lich 
  * @Date: 2019-10-18 10:19:23 
  * @Last Modified by: lich
- * @Last Modified time: 2019-10-18 13:52:32
+ * @Last Modified time: 2019-10-19 21:38:07
  * @TODO:
  * register：注册所有的待链接
  * next：如果对象池中存在可用实例，则执行下一个链接
@@ -10,8 +10,6 @@
  */
 
 const Pool = require("./Pool");
-const poolInstance = new Pool();
-
 const {spiderChapterInfo} = require('./spider');
 
 // class SpiderChapterInfo {
@@ -19,32 +17,36 @@ const {spiderChapterInfo} = require('./spider');
 //         return spiderChapterInfo(chapterInfoUrl)
 //     }
 // }
-new Array(poolInstance.maxConnection).fill(null).forEach(()=>{
-    // let instance = poolInstance.instantiate(SpiderChapterInfo);
-    poolInstance.put(spiderChapterInfo);
-});
 
-const store = [];
 
 function noop() {
     return void 0;
 }
 
 class ConnectionPool {
-    register(waitConnection, callback ) {
+    register(waitConnection, callback, seekDone ) {
+        this.poolInstance = new Pool();
+
+        new Array(this.poolInstance.maxConnection).fill(null).forEach(()=>{
+            this.poolInstance.put(spiderChapterInfo);
+        })
+
+
+        this.seekDone = seekDone || null;
         this.waitConnection = waitConnection || [];
         this.queryConnectionDoneId = null;
         this.callback = callback || noop ;
+        this.store = [];
     }
     next() {
-        console.log(poolInstance.size());
-        if (this.waitConnection.length > 0 && poolInstance.size() > 0) {
-           let instance = poolInstance.get();
+        console.log(this.poolInstance.size());
+        if (this.waitConnection.length > 0 && this.poolInstance.size() > 0) {
+           let instance = this.poolInstance.get();
            let params = this.waitConnection.shift();
 
            instance(params.url).then(content=>{
-            poolInstance.put(instance);
-            store.push(Object.assign(params, {content: content}));
+            this.poolInstance.put(instance);
+            this.setStore(Object.assign(params, {content: content}));
            })
         } else {
             console.log("对象池被占用完，等待......................");
@@ -55,15 +57,26 @@ class ConnectionPool {
         this.queryConnectionDoneId = this.queryConnectionDone();
     }
     close() {
-        this.callback(store);
+        this.callback(store, 'done');
         this.waitConnection = null;
         this.queryConnectionDone = null;
         this.callback = null;
     }
+    setStore(resultItem) {
+        if (this.store.length > 20) {
+            if (typeof this.seekDone === "function") { 
+                this.seekDone(this.store);
+                this.store = [];
+            }
+        }
+        this.store.push(resultItem);
+    }
     queryConnectionDone() {
+
+        
        let queryConnectionDoneId = setInterval(() => {
             if (this.waitConnection.length === 0) {
-                if (poolInstance.size() === poolInstance.maxConnection) {
+                if (this.poolInstance.size() === this.poolInstance.maxConnection) {
                     clearInterval(queryConnectionDoneId);
                     this.close();
                 }
