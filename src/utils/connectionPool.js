8c19d7ddd47c9e2cefe8f26cd1ed62e389167a68
lich <100108350@qq.com>
2019-10-21 11:20:27 +0800

    style: 添加注解

--- src/utils/connectionPool.js
+++ src/utils/connectionPool.js
@@ -2,7 +2,7 @@
  * @Author: lich 
  * @Date: 2019-10-18 10:19:23 
  * @Last Modified by: lich
- * @Last Modified time: 2019-10-19 21:38:07
+ * @Last Modified time: 2019-10-21 11:16:56
  * @TODO:
  * register：注册所有的待链接
  * next：如果对象池中存在可用实例，则执行下一个链接
@@ -11,7 +11,6 @@
 
 const Pool = require("./Pool");
 const {spiderChapterInfo} = require('./spider');
-
 // class SpiderChapterInfo {
 //     start(chapterInfoUrl) {
 //         return spiderChapterInfo(chapterInfoUrl)
@@ -24,46 +23,83 @@ function noop() {
 }
 
 class ConnectionPool {
-    register(waitConnection, callback, seekDone ) {
+    constructor() {
+        this.createFileCount = 20;
+    }
+
+    register(waitConnection, callback, seekDone) {
+        /**创建对象池 */
         this.poolInstance = new Pool();
 
+        /**向对象池中添加原始数据 */
         new Array(this.poolInstance.maxConnection).fill(null).forEach(()=>{
             this.poolInstance.put(spiderChapterInfo);
         })
 
-
-        this.seekDone = seekDone || null;
+        /**需要处理等待的链接 */
         this.waitConnection = waitConnection || [];
+        /**控制 开启链接查询 和 链接结束的 定时器id */
         this.queryConnectionDoneId = null;
+
+        /**当当前链接全部完成触发的回调函数 */
         this.callback = callback || noop ;
+
+        /**当缓存溢出时抛给seekDone */
+        this.seekDone = seekDone || null;
+        
         this.store = [];
     }
+
+    /**开启链接，如果当前等待连接存在， 并且 对象池中有可用的对象， 
+     * 则使用对象来开启一个新的连接 
+     * 当连接结束后 将对象返回给对象池给其他需要的待连接使用。
+     * */
     next() {
-        console.log(this.poolInstance.size());
+        // console.log(this.poolInstance.size());
         if (this.waitConnection.length > 0 && this.poolInstance.size() > 0) {
            let instance = this.poolInstance.get();
            let params = this.waitConnection.shift();
 
            instance(params.url).then(content=>{
             this.poolInstance.put(instance);
+            
             this.setStore(Object.assign(params, {content: content}));
+            // this.everyConnectionDone({code: 1, params: Object.assign(params, {content: content})});
            })
         } else {
             console.log("对象池被占用完，等待......................");
         }
     }
+
+    // /**任何一个连接执行结束后回调的函数 */
+    // everyConnectionDone({code, params}) {
+    //     if(code === 1){
+    //         generateFiles({chapterInfo:params, bookName: selectedNovel.name, path: fileAddress})
+    //     }
+    // }
+
     start() {
         this.next();
         this.queryConnectionDoneId = this.queryConnectionDone();
     }
     close() {
-        this.callback(store, 'done');
+        this.callback(this.store, 'done');
         this.waitConnection = null;
         this.queryConnectionDone = null;
         this.callback = null;
+        this.store = [];
     }
+
+    /**设置每缓存多少个文件生成，本地对应的文件
+     * @param { number } num 
+     */
+    setFileCreateCount(num) {
+        this.createFileCount = num;
+    }
+
+    /**缓存当前请求的结果，如果有需要则抛出 */
     setStore(resultItem) {
-        if (this.store.length > 20) {
+        if (this.store.length >= this.createFileCount) {
             if (typeof this.seekDone === "function") { 
                 this.seekDone(this.store);
                 this.store = [];
@@ -71,15 +107,18 @@ class ConnectionPool {
         }
         this.store.push(resultItem);
     }
-    queryConnectionDone() {
 
-        
+    /**查询是否还有等待的链接，如果有则继续开启下一个链接，如果没有关闭销毁当前实例 */
+    queryConnectionDone() {
        let queryConnectionDoneId = setInterval(() => {
             if (this.waitConnection.length === 0) {
                 if (this.poolInstance.size() === this.poolInstance.maxConnection) {
                     clearInterval(queryConnectionDoneId);
                     this.close();
-                }
+                } 
+                // else if (this.createFileCount > this.poolInstance.size()) {
+                //     this.close();
+                // }
             } else {
                 this.next();
             }
